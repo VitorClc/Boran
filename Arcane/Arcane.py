a61b9c5393c9f875562881ee41919b3b19880d41
Vitor Chaves <vitor.coelho2@fatec.sp.gov.br>
2019-08-25 20:19:12 -0300

    Added pygame

--- Arcane/Arcane.py
+++ Arcane/Arcane.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1270, 720))
+pygame.display.set_caption("Hello World!")
+
+done = False
+
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        done = True
+        
+        pygame.display.flip()
