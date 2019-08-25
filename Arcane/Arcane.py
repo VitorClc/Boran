932563c9b964a7245e2cece5af39663ba6da64cf
Vitor Chaves <vitor.coelho2@fatec.sp.gov.br>
2019-08-25 20:32:36 -0300

    Added a window center config

--- Arcane/Arcane.py
+++ Arcane/Arcane.py
@@ -1,7 +1,18 @@
 import pygame
+import pyautogui
+import os
+
+windowWidth = 800
+windowHeight = 600
+
+### Center Window
+x = (pyautogui.size().width - windowWidth) / 2
+y = (pyautogui.size().height - windowHeight) / 2
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
+###
 
 pygame.init()
-screen = pygame.display.set_mode((1270, 720))
+screen = pygame.display.set_mode((windowWidth, windowHeight))
 pygame.display.set_caption("Hello World!")
 
 done = False
