906e039588089e1c289d62000845465208567871
Vitor Chaves <vitor.coelho2@fatec.sp.gov.br>
2019-08-26 23:51:47 -0300

    Started the TileMap class

--- Arcane/testScene.py
+++ Arcane/testScene.py
@@ -9,6 +9,3 @@ class testScene(SceneModel):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                     running = False
-
-    def Update(self):
-        pygame.draw.rect(self.gameWindow.display, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
