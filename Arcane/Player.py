9c0462d9ce678f622012abbb12006c4155f2f8bc
Vitor Chaves <vitor.coelho2@fatec.sp.gov.br>
2019-09-01 16:32:30 -0300

    Started the player class

---
+++ Arcane/Player.py
@@ -0,0 +1,10 @@
+import pygame
+from Core.GameObject import GameObjectBase
+
+class Player(GameObjectBase):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def Update(self, _gameWindow):
+        pygame.draw.rect(_gameWindow, (0,0,255), (self.x,self.y, 64, 64))
--- Arcane/SCENES/testScene.py
+++ Arcane/SCENES/testScene.py
@@ -1,11 +1,14 @@
 import pygame
 from Core.Scenes import SceneModel
 from Core.Tilemap import Loader
+from Player import Player
 
 class testScene(SceneModel):
     def Start(self, _gameWindow):
         self.window = _gameWindow
         self.tilemap = Loader("MAPS/testScene.tmx")
+        self.player = Player(0,0)
 
     def Update(self):
         self.tilemap.render(self.window.display)
+        self.player.Update(self.window.display)
