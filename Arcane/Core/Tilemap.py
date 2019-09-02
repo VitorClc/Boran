43922a73f3a20bdcde74ce303930d912bab22329
Vitor Chaves <vitor.coelho2@fatec.sp.gov.br>
2019-09-02 01:14:40 -0300

    Added a multilayer tilemap rendering

--- Arcane/Core/Tilemap.py
+++ Arcane/Core/Tilemap.py
@@ -5,22 +5,23 @@ class Loader(object):
     def __init__(self, _filename):
         self.filename = _filename
         self.tilemapData = load_pygame(self.filename)
-
         #self.tileWidth = self.tilemapData.tilewidth    
         #self.tileHeight = self.tilemapData.tileheight
 
         #### MAP INFO
         self.width = self.tilemapData.width
         self.height = self.tilemapData.height
+        self.layers = self.tilemapData.layers
 
     def Render(self, display):
-        for x in range(self.width):
-            for y in range(self.height):
-                tile = self.tilemapData.get_tile_image(x,y,0)
-                xPos = (x * 256 / 2) + (y * 256 / 2)
-                yPos = (y * 128 / 2) - (x * 128 / 2)
-                resizedTile = pygame.transform.scale(tile, (128, 256))
-                if(tile != None):
-                    display.blit(resizedTile, (xPos / 2, yPos / 2))
-
+        for layer in range(len(self.layers)):
+            for x in range(0, self.width):
+                for y in range(0,self.height):
+                    tile = self.tilemapData.get_tile_image(x,y,layer)
+                    if(tile != None):
+                        xPos = (x * 256 / 2) - (y * 256 / 2)
+                        yPos = (y * 128 / 2) + (x * 128 / 2)
+                        centeredX = xPos + display.get_rect().centerx
+                        centeredY = yPos - display.get_rect().centery
+                        display.blit(tile, (centeredX, centeredY))
 
