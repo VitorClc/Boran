882be7bc577fc71bae3b53fec9335a0538d404f1
Vitor Chaves <vitor.coelho2@fatec.sp.gov.br>
2019-09-02 03:10:59 -0300

    Added limits and sensibility to the camera

--- Arcane/Core/Camera.py
+++ Arcane/Core/Camera.py
@@ -1,19 +1,28 @@
 import pygame
 
 class MouseControl(object):
-    def __init__(self):
-        self.velx = 0
-        self.vely = 0
+    def __init__(self, xlimits, ylimits, sensibility):
+        self.x = 0
+        self.y = 0
+
+        self.xMin = xlimits[0]
+        self.xMax = xlimits[1]
+
+        self.yMin = ylimits[0]
+        self.yMax = ylimits[1]
+        
+        self.sensibility = sensibility
 
     def getMovements(self, display):
+
         mousex, mousey = pygame.mouse.get_pos()
 
-        if(mousex < 50):
-            self.velx += 5
-        elif(mousex > display.get_rect().width - 50):
-            self.velx -= 5
+        if(mousex < 50 and self.x <= self.xMin):
+            self.x += self.sensibility
+        elif(mousex > display.get_rect().width - 50 and self.x >= self.xMax):
+            self.x -= self.sensibility
 
-        if(mousey < 50):
-            self.vely += 5
-        elif(mousey > display.get_rect().height - 50):
-            self.vely -= 5
+        if(mousey < 50 and self.y <= self.yMin):
+            self.y += self.sensibility
+        elif(mousey > display.get_rect().height - 50 and self.y >= self.yMax):
+            self.y -= self.sensibility
--- Arcane/SCENES/testScene.py
+++ Arcane/SCENES/testScene.py
@@ -7,12 +7,11 @@ from Player import Player
 class testScene(SceneModel):
     def Start(self, _gameWindow):
         self.window = _gameWindow
-        self.camera = MouseControl()
+        self.camera = MouseControl([500, -700], [500, -700], 10)
         self.tilemap = Loader("MAPS/testScene.tmx")
         #self.player = Player(0,0)
 
     def Update(self):
         self.camera.getMovements(self.window.display)
-
-        self.tilemap.Render(self.window.display, self.camera.velx, self.camera.vely)
+        self.tilemap.Render(self.window.display, self.camera.x, self.camera.y)
         #self.player.Render(self.window.display)
