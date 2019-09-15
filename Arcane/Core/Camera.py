f55605d0ef0d39beb93f2c3e3d2e6e3b20ac4092
Vitor Chaves <vitor.coelho2@fatec.sp.gov.br>
2019-09-15 10:25:50 -0300

    Added a Camera following

--- Arcane/Core/Camera.py
+++ Arcane/Core/Camera.py
@@ -1,28 +1,11 @@
 import pygame
 
-class MouseControl(object):
-    def __init__(self, xlimits, ylimits, startPosition, sensibility):
+class PlayerFollow(object):
+    def __init__(self, startPosition):
         self.x = startPosition[0]
         self.y = startPosition[1]
 
-        self.xMin = xlimits[0]
-        self.xMax = xlimits[1]
+    def getPlayerPosition(self, playerPos):
+        self.x = -playerPos.x + 850
+        self.y = -playerPos.y + 150
 
-        self.yMin = ylimits[0]
-        self.yMax = ylimits[1]
-        
-        self.sensibility = sensibility
-
-    def getMovements(self, display):
-
-        mousex, mousey = pygame.mouse.get_pos()
-
-        if(mousex < 50 and self.x <= self.xMin):
-            self.x += self.sensibility
-        elif(mousex > display.get_rect().width - 50 and self.x >= self.xMax):
-            self.x -= self.sensibility
-
-        if(mousey < 50 and self.y <= self.yMin):
-            self.y += self.sensibility
-        elif(mousey > display.get_rect().height - 50 and self.y >= self.yMax):
-            self.y -= self.sensibility
--- Arcane/SCENES/testScene.py
+++ Arcane/SCENES/testScene.py
@@ -1,7 +1,7 @@
 import pygame
 from Core.Scenes import SceneModel
 from Core.Tilemap import Loader
-from Core.Camera import MouseControl
+from Core.Camera import PlayerFollow
 from Core.Text import Text
 
 from Player import Player
@@ -13,15 +13,13 @@ class testScene(SceneModel):
         self.tilemap = Loader(self.window.display, "MAPS/testScene.tmx", 4, pygame.math.Vector2(0,-1))
         self.tilemap.Render()
 
-        self.camera = MouseControl([1000, -0], [500, -700], [800,0], 10)
-
         self.player = Player(self.window.display, self.tilemap.isometricToCartesian(pygame.Vector2(0,0)), self.tilemap.map)
-
+        self.camera = PlayerFollow(self.player.cartesianPos)
+        
         self.mousePosIsoText = Text("Mouse ISO", 25, pygame.Vector2(0, 0))
         self.playerPosText = Text("Player position", 25, pygame.Vector2(0, 26))
 
     def Update(self):
-        self.camera.getMovements(self.window.display)
         isoClickPos = self.tilemap.cartesianToIsometric(pygame.math.Vector2(pygame.mouse.get_pos()[0] - self.camera.x, - self.camera.y + pygame.mouse.get_pos()[1] - (self.tilemap.yOffset * self.tilemap.tileSize.y) + 64), self.camera)
         self.mousePosIsoText.setText("Mouse ISOMETRIC: " + str(isoClickPos))
 
@@ -34,6 +32,8 @@ class testScene(SceneModel):
                     self.player.path, runs = self.player.finder.find_path(start, end, self.player.grid)
                     #print(self.player.grid.grid_str(path=self.player.path, start=start, end=end))
                     self.player.goToPosition()
+
+        self.camera.getPlayerPosition(self.player.isoMov)
 
         self.playerPosText.setText(self.player.isoReal)
 
