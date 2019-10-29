9e8955c67ad79829c013349329686d96bcd1e952
Vitor Chaves <vitor.coelho2@fatec.sp.gov.br>
2019-10-29 20:28:16 -0300

    Added a tilemap rendering

--- Arcane/Core/Tilemap.py
+++ Arcane/Core/Tilemap.py
@@ -7,13 +7,14 @@ wall = pygame.image.load(tileDir + 'block.png')
 ground = pygame.image.load(tileDir + 'floor.png')
 
 class Loader():
-    def __init__(self, gameWindow, _filename, groundGroup):
+    def __init__(self, gameWindow, _filename, groundGroup, wallGroup):
         filename = _filename
 
         self.window = gameWindow
         
         self.groundGroup = groundGroup
-
+        self.wallGroup = wallGroup
+        
         self.tilemapData = load_pygame(filename, pixelalpha=True)
 
         self.tileSize = pygame.Vector2(self.tilemapData.tilewidth, self.tilemapData.tileheight)
@@ -35,6 +36,26 @@ class Loader():
                     centered_y = self.window.get_rect().centery/2
 
                     Tile(self.groundGroup, ground.convert_alpha(), 0, (xPos + centered_x, yPos - centered_y))
+        
+        for x in range(0, int(self.mapSize.x)):
+            for y in range(0, int(self.mapSize.y)):
+                tile = self.tilemapData.get_tile_properties(x,y,1)
+                if(tile != None):
+                    xPos = (x - y) * self.tileSize.x / 2
+                    yPos = (y + x) * self.tileSize.y / 2
+
+                    centered_x = self.window.get_rect().centerx
+                    centered_y = self.window.get_rect().centery/2
+
+                    Tile(self.wallGroup, wall.convert_alpha(), 1, (xPos + centered_x, yPos - centered_y))
+
+
+    def Update(self, camera):
+        #for i in len(self.groundGroup.sprites):
+        #    print(i)
+
+        #self.wallGroup.update()
+        pass
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, group, image, layer, pos):
--- Arcane/SCENES/testScene.py
+++ Arcane/SCENES/testScene.py
@@ -12,10 +12,25 @@ class testScene(SceneModel):
         self.ground = pygame.sprite.LayeredUpdates()
         self.wall = pygame.sprite.LayeredUpdates()
 
-        self.tilemap = Loader(self.window.display, "MAPS/testScene.tmx", self.ground)
+        self.tilemap = Loader(self.window.display, "MAPS/testScene.tmx", self.ground, self.wall)
+        self.camera = pygame.Vector2(0,0)
+        
+    def ProcessInput(self, event, pressed_keys):
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_w]:
+            self.camera.y += 5
+        if keys[pygame.K_a]:
+            self.camera.x += 5
+        if keys[pygame.K_s]:
+            self.camera.y -= 5
+        if keys[pygame.K_d]:
+            self.camera.x -= 5
 
     def Update(self):
-        self.ground.update()
+        #self.tilemap.Update(self.camera)
+        pass
         
     def Render(self):
         self.ground.draw(self.window.display)
+        self.wall.draw(self.window.display)
